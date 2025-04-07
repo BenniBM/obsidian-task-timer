f585087164a1e2cc0df67d2fced75d8caf4315d5
Benjamin Mehl <Benni.mehlBM@gmail.com>
2025-04-07 18:38:34 +0200

    multiple timers

--- src/ui/TimerDisplay.svelte
+++ src/ui/TimerDisplay.svelte
@@ -51,8 +51,12 @@
     <div
         class="fixed bottom-5 right-5 bg-background-secondary border border-background-modifier-border rounded-xl p-8 z-50 max-w-[325px] min-w-[325px] shadow-lg animate-slide-up"
     >
-        {#each Array.from(timers) as [timerId, timer] (timerId)}
-            <div class="mb-2.5">
+        {#each Array.from(timers) as [timerId, timer], index}
+            <div
+                class={index !== 0
+                    ? "border-t border-background-modifier-border mt-6 pt-6"
+                    : ""}
+            >
                 <h2
                     class="text-3xl text-white font-bold tracking-wide {getTimerClass(
                         timer,
@@ -61,12 +65,12 @@
                     {getTimeDisplay(timer)}
                 </h2>
                 <span
-                    class="mr-3 flex-1 text-sm block truncate"
+                    class="mr-3 flex-1 mb-4 text-sm block truncate"
                     title={timer.description}>{timer.description}</span
                 >
                 <div class="flex justify-start mt-2 gap-2">
                     <button
-                        class="px-4 py-1 rounded-md border-2 border-background-modifier-border bg-background-primary cursor-pointer text-sm transition-all duration-200 hover:bg-background-modifier-hover hover:-translate-y-0.5"
+                        class="px-4 py-1 rounded-md cursor-pointer text-sm transition-all duration-200 hover:bg-background-modifier-hover hover:-translate-y-0.5"
                         on:click={() =>
                             timer.isPaused
                                 ? timerStore.resumeTimer(timerId, () => {})
@@ -75,7 +79,7 @@
                         {timer.isPaused ? "Resume" : "Pause"}
                     </button>
                     <button
-                        class="px-4 py-1 rounded-md border-2 border-text-error text-error bg-background-primary cursor-pointer text-sm transition-all duration-200 hover:bg-text-error hover:text-background-primary hover:-translate-y-0.5"
+                        class="px-4 py-1 rounded-md bg-background-primary cursor-pointer text-sm transition-all duration-200 hover:bg-text-error hover:text-background-primary hover:-translate-y-0.5"
                         on:click={() => timerStore.cancelTimer(timerId)}
                     >
                         Delete
